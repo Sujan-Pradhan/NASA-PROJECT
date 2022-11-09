15a4c40d7c53f9c8cca1e04c8aefdb49302b2df0
Sujan <pradhansujan730@gmail.com>
2022-11-09 22:37:39 +0545

    NASA-PROJECT client and server folder upload

---
+++ server/src/server.js
@@ -0,0 +1,22 @@
+const http = require("http")
+const app = require("./app")
+
+const {loadPlanetsData} = require("./models/planets.model")
+
+const port = process.env.PORT || 8000;
+
+const server = http.createServer(app);
+
+async function startServer(){
+    await loadPlanetsData();
+
+    server.listen(port,()=>{
+        console.log(`Server listening on port ${port}`)
+    })
+}
+
+startServer()
+
+
+
+// Benefits => organize the code a letter more 
