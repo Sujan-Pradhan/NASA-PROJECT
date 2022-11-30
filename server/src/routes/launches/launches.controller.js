b4a2a55b1c2c3edd368a556c9da9e833bbaa2068
Sujan <pradhansujan730@gmail.com>
2022-11-30 23:16:44 +0545

    Make auto increment in mongo and convert model and cotroller working on mongo

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,15 +1,16 @@
 const {
   getAllLaunches,
-  addNewLaunch,
+  // addNewLaunch,
   existsLaunchedWithId,
   abortLaunchById,
+  scheduleNewlaunch,
 } = require("../../models/launches.model");
 
 async function httpGetAllLaunches(req, res) {
   return res.status(200).json(await getAllLaunches());
 }
 
-function httpAddNewLaunch(req, res) {
+async function httpAddNewLaunch(req, res) {
   const launch = req.body;
 
   if (
@@ -27,23 +28,33 @@ function httpAddNewLaunch(req, res) {
       error: "Invalid Launch Date",
     });
   }
-  addNewLaunch(launch);
+  // addNewLaunch(launch);
+  await scheduleNewlaunch(launch)
   return res.status(201).json(launch);
 }
 
-function httpAbortLaunch(req, res) {
+async function httpAbortLaunch(req, res) {
   const launchId = Number(req.params.id);
 
   //if launch doesn't exists
-  if (!existsLaunchedWithId(launchId)) {
+  const existsLaunch = await existsLaunchedWithId(launchId)
+  if (!existsLaunch) {
     return res.status(404).json({
       error: "Launch not found",
     });
   }
 
   //if launch does exist
-  const aborted = abortLaunchById(launchId);
-  return res.status(200).json(aborted);
+  const aborted = await abortLaunchById(launchId);
+  if(!aborted){
+    return res.status(400).json({
+      error:"Launch not aborted"
+    })
+  }
+  // return res.status(200).json(aborted);
+  return res.status(200).json({
+    ok:true
+  });
 }
 
 module.exports = {
